409d6ca53cd1f2a47e4b77b6d10e92a893c412cd
Yojiro Nemoto <yojironemoto@YojironoMacBook-Pro.local>
2021-07-12 16:27:41 +0900

    feat: Profileの型を定義

--- reqres/src/components/Profile.tsx
+++ reqres/src/components/Profile.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Icon from '../sample.jpg';
 
-interface ProfileProps {
+interface Props {
     name: string;
     email: string;
 }
 
-const Profile = (props: ProfileProps) => {
-    return (
+const Profile: React.FC<Props> = (props) => (
         <div className="profile">
             <h2>{props.name}</h2>
             <h3>{props.email}</h3>
             <img src={Icon} alt="アイコン" /> 
         </div>
-    );
-}
+);
+
 
 export default Profile;
