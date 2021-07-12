78b41599cf03a0de8c9f4c74b1d2ff7b9bb179de
muttsu-623 <mutsumi1996@gmail.com>
2021-07-12 10:48:25 +0900

    feat: add users

--- reqres/src/App.tsx
+++ reqres/src/App.tsx
@@ -2,7 +2,35 @@ import React from 'react';
 import './App.css';
 import Icon from './sample.jpg'
 
+interface User {
+  name: string;
+  email: string;
+}
+
 function App() {
+  const users: User[] = [
+    { name: 'name1', email: 'name1@example.com' },
+    { name: 'name2', email: 'name2@example.com' },
+    { name: 'name3', email: 'name3@example.com' },
+    { name: 'name4', email: 'name4@example.com' },
+    { name: 'name5', email: 'name5@example.com' },
+    { name: 'name6', email: 'name6@example.com' },
+    { name: 'name7', email: 'name7@example.com' },
+    { name: 'name8', email: 'name8@example.com' },
+    { name: 'name9', email: 'name9@example.com' },
+    { name: 'name10', email: 'name10@example.com' },
+    { name: 'name11', email: 'name11@example.com' },
+    { name: 'name12', email: 'name12@example.com' },
+    { name: 'name13', email: 'name13@example.com' },
+    { name: 'name14', email: 'name14@example.com' },
+    { name: 'name15', email: 'name15@example.com' },
+    { name: 'name16', email: 'name16@example.com' },
+    { name: 'name17', email: 'name17@example.com' },
+    { name: 'name18', email: 'name18@example.com' },
+    { name: 'name19', email: 'name19@example.com' },
+    { name: 'name20', email: 'name20@example.com' },
+  ];
+
   return (
     <div className="App">
       <h1>Hello ReqRes users!</h1>
